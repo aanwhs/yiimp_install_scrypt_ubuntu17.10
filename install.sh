11bd2a06c8ad437fcea4cdc5fadea870b7c94bb4
Tatar Xavatar <xavatar@wanadoo.fr>
2018-06-14 20:35:21 +0200

    fix typo

--- install.sh
+++ install.sh
@@ -131,12 +131,12 @@ default         0;
     sudo apt-get -y update
     sudo apt-get install -y libdb4.8-dev libdb4.8++-dev libdb5.3 libdb5.3++
 	
-	#Conf older Version of GCC
-	sudo apt-get install -y libidn2-dev
-	sudo apt-get install -y libpsl-dev
+    #Conf older Version of GCC
+    sudo apt-get install -y libidn2-dev
+    sudo apt-get install -y libpsl-dev
     sudo apt-get install -y gcc-5 g++-5
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-7
     
     #Generating Random Passwords
     password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
